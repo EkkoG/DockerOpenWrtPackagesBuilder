f83ba1dd0f23be546957b04bb25ec170d91eee44
openwrtddev <beijiu572@gmail.com>
2021-03-23 15:10:09 +0800

    make build script can build common packages

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 
-./scripts/feeds update luci
+sudo apt-get update
+sudo apt-get install upx -y
+cp /usr/bin/upx staging_dir/host/bin
+cp /usr/bin/upx-ucl staging_dir/host/bin
+
+./scripts/feeds update -a
+
+pushd feeds/packages/lang
+rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-21.02/lang/golang
+popd
+
+./scripts/feeds install luci-app-passwall
+
 make defconfig
-./scripts/feeds install luci
-make package/luci/{clean,compile} -j$(nproc)
+
+make package/luci-app-passwall/{clean,compile} -j4
+make package/index
